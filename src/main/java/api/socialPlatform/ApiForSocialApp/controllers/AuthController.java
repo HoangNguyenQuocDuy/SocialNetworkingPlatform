476d02f8239804b35623c2a5cb098de9557a9535
unknown <quocduy6114@gmail.com>
2023-10-13 02:49:26 -0700

    cors config

--- src/main/java/api/socialPlatform/ApiForSocialApp/controllers/AuthController.java
+++ src/main/java/api/socialPlatform/ApiForSocialApp/controllers/AuthController.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("api/v1/auth")
+@CrossOrigin()
 public class AuthController {
     private final AuthService authService;
 
